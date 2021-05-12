0a219ea544d6513f380e4fe11e4af163cdb1d38a
DrDeee <digitalis13@gmx.de>
2021-05-12 08:25:42 +0200

    optimize mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// Footer 
+
+const footer = document.querySelector('#footer')
+const smallFooter = document.querySelector('#footer div.small')
+const expandedFooter = document.querySelector('#footer div.expanded')
+
+footer.addEventListener('mouseover', () => {
+    smallFooter.style.display = 'none'
+    expandedFooter.style.display = 'block'
+})
+
+footer.addEventListener('mouseout', () => {
+    smallFooter.style.display = 'block'
+    expandedFooter.style.display = 'none'
+})
+
+// APP
 app = new Vue({
     el: '#app',
     data: {
